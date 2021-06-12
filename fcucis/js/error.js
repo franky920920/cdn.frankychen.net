45594ffced5d815565cfe32fda627f9d7985436e
Hsuan-Le Chen <franky920920+gpg@gmail.com>
2021-06-12 14:45:59 +0800

    Add fcucis resources

---
+++ fcucis/js/error.js
@@ -0,0 +1,8 @@
+document.getElementById('contact').addEventListener('click', () => {
+    //TODO: redirect
+    window.location = "/contact"
+})
+
+document.getElementById('home').addEventListener('click', () => {
+    window.location = "/";
+})
